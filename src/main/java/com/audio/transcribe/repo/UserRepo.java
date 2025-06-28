c9a58169408f232b36c394daec46f737bc17d816
nadunrandeera <nadunrandeera@gmail.com>
2025-06-28 22:45:32 +0530

    add UserEntity and UserRepo

---
+++ src/main/java/com/audio/transcribe/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.audio.transcribe.repo;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
